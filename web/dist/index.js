9a87f9556427ae41b3baa88af12f900ae6d0bc60
Chanadu <chandu.peddada@gmail.com>
2025-06-23 15:28:14 -0500

    changes ig

--- web/dist/index.js
+++ web/dist/index.js
@@ -1,21 +1,33 @@
-const input = document.getElementById("bplInput");
-const button = document.getElementById("runButton");
-const output = document.getElementById("bplOutput");
-window.goPrint = function (msg) {
-    console.log("Go Print: " + msg);
-    output.textContent += msg;
+const input = document.getElementById('bplInput');
+const button = document.getElementById('runButton');
+const output = document.getElementById('bplOutput');
+window.goPrint = function (msg, isError) {
+    if (isError) {
+        console.log('Go Error: ' + msg);
+    }
+    else {
+        console.log('Go Print: ' + msg);
+    }
+    if (isError) {
+        output.textContent += '<span style="color: red;">' + msg + '</span>';
+    }
+    else {
+        output.textContent += msg;
+    }
+    // output.textContent += '<span style="color: red;">' + msg + '</span>';
+    // output.textContent += '<span style="color: red;">' + msg + '</span>';
 };
 function runGo(code) {
     const go = new Go();
-    go.argv = ["better-language", code];
-    WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
-        console.log("Starting Wasm");
+    go.argv = ['better-language', code];
+    WebAssembly.instantiateStreaming(fetch('main.wasm'), go.importObject).then((result) => {
+        console.log('WASM');
         go.run(result.instance);
     });
 }
-button.addEventListener("click", () => {
-    console.log("Run button clicked");
-    output.textContent = ""; // Clear previous output
+button.addEventListener('click', () => {
+    console.log('Run button clicked');
+    output.textContent = ''; // Clear previous output
     const code = input.value.trim();
     runGo(code);
 });
--- web/index.ts
+++ web/index.ts
@@ -1,34 +1,43 @@
 export {};
 
 declare global {
-  interface Window {
-    goPrint: (msg: string) => void;
-  }
+	interface Window {
+		goPrint: (msg: string, isError: boolean) => void;
+	}
 }
 
-const input = document.getElementById("bplInput")! as HTMLTextAreaElement;
-const button = document.getElementById("runButton")!;
-const output = document.getElementById("bplOutput")!;
+const input = document.getElementById('bplInput')! as HTMLTextAreaElement;
+const button = document.getElementById('runButton')!;
+const output = document.getElementById('bplOutput')!;
 
-window.goPrint = function (msg: string) {
-  console.log("Go Print: " + msg);
-  output.textContent += msg;
+window.goPrint = function (msg: string, isError: boolean) {
+	if (isError) {
+		console.log('Go Error: ' + msg);
+	} else {
+		console.log('Go Print: ' + msg);
+	}
+	if (isError) {
+		output.textContent += '<span style="color: red;">' + msg + '</span>';
+	} else {
+		output.textContent += msg;
+	}
+	// output.textContent += '<span style="color: red;">' + msg + '</span>';
+	// output.textContent += '<span style="color: red;">' + msg + '</span>';
 };
 
 function runGo(code: string) {
-  const go = new Go();
-  go.argv = ["better-language", code];
-  WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then(
-    (result) => {
-      console.log("Starting Wasm");
-      go.run(result.instance);
-    },
-  );
+	const go = new Go();
+
+	go.argv = ['better-language', code];
+	WebAssembly.instantiateStreaming(fetch('main.wasm'), go.importObject).then((result) => {
+		console.log('WASM');
+		go.run(result.instance);
+	});
 }
 
-button.addEventListener("click", () => {
-  console.log("Run button clicked");
-  output.textContent = ""; // Clear previous output
-  const code = input.value.trim();
-  runGo(code);
+button.addEventListener('click', () => {
+	console.log('Run button clicked');
+	output.textContent = ''; // Clear previous output
+	const code = input.value.trim();
+	runGo(code);
 });
